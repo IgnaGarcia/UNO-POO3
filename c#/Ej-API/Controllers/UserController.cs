9f74c91fe575cbe3f7d3d7da1f0334db07eb6361
Igna Garcia <gnachoxp@gmail.com>
2021-10-21 21:20:51 -0300

    clase de api

---
+++ c#/Ej-API/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Ej_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ej_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public User[] UserList = new[]
+        {
+            new User("igna", "garcia", 22),
+            new User("juanchi", "garcia", 02),
+            new User("palo", "garcia", 14),
+            new User("cata", "jonas", 23)
+        };
+
+        [HttpGet]
+         public ActionResult<User[]> GetAll()
+        {
+            return Ok(UserList);
+         }
+
+        [HttpGet("{id}")]
+        public ActionResult<User> Get(int id)
+        {
+            return Ok(UserList.GetValue(id));
+        }
+    }
+}
---
+++ c#/Ej-API/Models/User.cs
@@ -0,0 +1,21 @@
+﻿namespace Ej_API.Models
+{
+    public class User
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public int edad { get; set; } 
+
+        public User(string nombre, string apellido, int edad)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.edad = edad;
+        }
+
+        override public string ToString()
+        {
+            return this.nombre;
+        }
+    }
+}
